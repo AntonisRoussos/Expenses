63bdff49445c981b7c3cfc18929c6f9ac929cf68
Antonis Roussos <grtramp@yahoo.gr>
2012-03-22 19:52:11 +0200

    restructuring 1

---
+++ assets/www/Javascripts/expensedetails.js
@@ -0,0 +1,17 @@
+var id = getUrlVars()["id"];
+ function getUrlVars() {
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	    for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+	    return vars;
+ }
+ 
+  function showtask() {
+	    location.href= '#deletedialog(' + getUrlVars()["id"] + ')';
+	    return false;
+ }
